7e5574034726d8be8b718acdc209d8b96f3a8764
Alex <example@gmail.com>
2020-06-06 15:09:32 +0300

    arrays

--- Study/objects/app.js
+++ Study/objects/app.js
@@ -1,40 +1,35 @@
-const options = {
-  name: 'test',
-  width: 1024,
-  height: 1024,
-  colors: {
-    border: 'black',
-    bg: 'red'
-  },
-  makeTest: function () {
-    console.log('Test');
-  }
-};
+const arr = [2, 13, 26, 8, 15];
 
-options.makeTest();
+arr.sort(compareNum);
+console.log(arr);
 
-const {
-  border,
-  bg
-} = options.colors;
-console.log(border);
+function compareNum(a, b) {
+  return a - b;
+}
 
-// console.log(Object.keys(options).length);
+// console.log(arr.length);
 
-// console.log(options.name);
+// arr[99] = 0;
 
-// delete options.name;
+// arr.pop();
 
-// console.log(options);
-// let counter = 0;
-// for (let key in options) {
-//   if (typeof (options[key]) === 'object') {
-//     for (let i in options[key]) {
-//       console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-//     }
-//   } else {
-//     console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//     counter++;
-//   }
+// arr.push(10);
+
+// console.log(arr);
+
+// arr.forEach(function (item, i, arr) {
+//   console.log(`${i}: ${item} внутри массива  ${arr}`);
+// });
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
 // }
-// console.log(counter);
+
+// for (let value of arr) {
+//   console.log(value);
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join("; "));
